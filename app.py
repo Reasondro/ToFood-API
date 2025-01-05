659eb704dcda7f5c15aff08a79f109b494b4e182
Reasondro <alessandrohasian16@gmail.com>
2025-01-05 20:58:50 +0700

    add api key db implement

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from jose import JWTError, jwt
 from pydantic import BaseModel
 import bcrypt
 from llama_cpp import Llama
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from uuid import uuid4
+import secrets
 
 my_model_path = "./model/unsloth.Q4_K_M.gguf"
 CONTEXT_SIZE = 30000
@@ -13,7 +16,6 @@ CONTEXT_SIZE = 30000
 tofood_model = Llama(model_path=my_model_path,n_ctx=CONTEXT_SIZE)
 
 app = FastAPI()
-
 
 class User(BaseModel):
     username: str
@@ -24,6 +26,18 @@ class UserInDB(User):
 class PromptRequest(BaseModel):
     instruction: str
     input: str
+
+
+class APIKeys(SQLModel, table =True):
+    id: str= Field(primary_key=True)
+    name: str = Field(index=True)
+    api_key: str 
+
+# TODO implement like api keys (database)
+class Customers(SQLModel, table =True):
+    id: str = Field(primary_key=True)
+    name: str = Field(index=True)
+    hashed_password: str
 
 
 def get_password_hash(password):
@@ -72,35 +86,6 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-
-@app.get("/sample")
-async def index():
-    return {
-        "info": "Try /hello/Sandro for parameterized route.",
-    }
-    
-@app.get("/hello/{name}")
-async def get_name(name: str):
-    return {
-        "name": name,
-    }
-    
-
-# ? endpoint for tokens
-@app.post("/api/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials",
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
 # ? function for dependency/session
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -119,46 +104,118 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         raise credentials_exception
     return user
 
+api_key_header = APIKeyHeader(name ="X-API-KEY")
+
+def get_user_with_api_key(api_key: str = Security(api_key_header)):
+    if not api_key:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Missing X-API-KEY header"
+        )
+    with Session(engine) as session:
+        result = session.exec(
+            select(APIKeys).where(APIKeys.api_key == api_key)
+        ).first()
+        if not result:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid API key"
+            )
+        return result
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.get("/sample")
+async def index():
+    return {
+        "info": "Try /hello/Sandro for parameterized route.",
+    }
+    
+@app.get("/hello/{name}")
+async def get_name(name: str):
+    return {
+        "name": name,
+    }
+    
+@app.post("/api/customers")
+def create_customer(customer: Customers):
+    with Session(engine) as session:
+        session.add(customer)
+        session.commit()
+        session.refresh(customer)
+        return customer
+    
+@app.get("/api/customers")
+def read_customers():
+    with Session(engine) as session:
+        customers = session.exec(select(Customers)).all()
+        return customers
+    
+
+# ? endpoint for tokens
+@app.post("/api/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
 # ? protected route for testing
 @app.get("/api/protected-route")
 async def read_protected_route(current_user: User = Depends(get_current_user)):
     return {"message": f"Hello, {current_user.username}!"}
 
+# ? generate api key stuffs
+class APIKeyGenerateRequest(BaseModel):
+    name: str
+    
+@app.post("/api/generate-api-key")
+def generate_api_key(req: APIKeyGenerateRequest):
+    new_api_key_value = secrets.token_hex(16)
+    new_id = str(uuid4())
 
-
-
-#  api key stuffs starts here
-api_key_header = APIKeyHeader(name ="X-API-KEY")
-
-api_keys = {
-    "e54d4431-5dab-474e-b71a-0db1fcb9e659": "7oDYjo3d9r58EJKYi5x4E8",
-    "5f0c7127-3be9-4488-b801-c7b6415b45e9": "mUP7PpTHmFAkxcQLWKMY8t"
-}
-users = {
-    "7oDYjo3d9r58EJKYi5x4E8": {
-        "name": "Bob"
-    },
-    "mUP7PpTHmFAkxcQLWKMY8t": {
-        "name": "Alice"
-    },
-}
-def check_api_key(api_key: str):
-    return api_key in api_keys
-
-def get_user_from_api_key_db(api_key: str):
-    return users[api_keys[api_key]]
-
-def get_user_with_api_key(api_key_header: str = Security(api_key_header)):
-    if check_api_key(api_key_header):
-        user = get_user_from_api_key_db(api_key_header)
-        return user
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Missing or invalid API key"
+    new_api = APIKeys(
+        id=new_id,
+        name=req.name,
+        api_key=new_api_key_value
     )
-# api key stuffs ends here
+    with Session(engine) as session:
+        session.add(new_api)
+        session.commit()
+        session.refresh(new_api)
+    return {
+        "message": f"API key created for {req.name or 'Unnamed Service'}",
+        "id": new_id,
+        "api_key": new_api_key_value
+    }
+    
+# ? see api keys list
+@app.get("/api/api-keys")
+def read_api_keys():
+    with Session(engine) as session:
+        apikeys = session.exec(select(APIKeys)).all()
+        return apikeys
 
-#? main endpoint for business logic
+# ? main stuffs
 @app.post("/api/prompt")
 async def get_prompt(request_body: PromptRequest, _ :dict = Depends(get_user_with_api_key) ):
 
